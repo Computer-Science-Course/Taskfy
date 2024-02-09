ec14d5f5cd50b559bfef660284ff3894cce3fe56
Neill-Ataide <newdonataide2k19@gmail.com>
2024-02-08 22:37:59 -0400

    add login, usuario, senha

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,4 +1,25 @@
-import { Text, View } from "react-native";
+// import { Text, View } from "react-native";
+
+// import { theme } from "../../config";
+// import { colors } from "../../config/styles";
+// import useStyles from "./styles";
+
+// const Login = () => {
+//   /** As cores estão configuradas, basta usá-las dentro do arquivo styles.js */
+//   const classes = useStyles(colors(theme));
+
+//   return (
+//     /** Pra usar o estilo, basta chamar ele como no exemplo abaixo. */
+//     <View style={classes.container}>
+      
+//       <Text style={classes.text}>Login</Text>
+//     </View>
+//   );
+// }
+
+// export default Login;
+
+import { Text, View, StyleSheet, TextInput } from "react-native";
 
 import { theme } from "../../config";
 import { colors } from "../../config/styles";
@@ -11,8 +32,11 @@ const Login = () => {
   return (
     /** Pra usar o estilo, basta chamar ele como no exemplo abaixo. */
     <View style={classes.container}>
-      {/* Isso é um comentário! Aqui dentro do view vai o conteúdo da tela. */}
       <Text style={classes.text}>Login</Text>
+      <Text style={classes.text_usuario}>Usuário</Text>
+      <TextInput>Digite o usuário</TextInput>
+      <Text style={classes.text_senha}>Senha</Text>
+      <TextInput>Digite a senha</TextInput>
     </View>
   );
 }
