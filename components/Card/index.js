83bc9ac0d50dea623866b0be0ec51399e5d8c8d5
Mateus Santos <a.mateussm1.5@gmail.com>
2024-02-09 19:29:13 -0400

    Add card.

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,40 +1,61 @@
-import { Text, View } from "react-native";
+import { TextInput, View } from "react-native";
 
 import { theme } from "../../config";
 import { colors as useColors } from "../../config/styles";
 import useStyles from "./styles";
 import Button from "../Button";
-import { ChevronDown, Play } from 'lucide-react-native'
+import { ChevronDown, Pencil, Play, Trash } from 'lucide-react-native'
+
+const itemSize = 24;
 
 const Card = () => {
   const colors = useColors(theme);
-  const classes = useStyles(colors);
+  const classes = useStyles({colors, itemSize});
 
   return (
-    <>
-      <View style={classes.containerCard}>
-        <Button
-          title="Baixa"
-          bgColor={colors.secondary.bg}
+    <View style={classes.containerCard}>
+      <View style={classes.cardHeader}>
+        <Pencil
           color={colors.white}
-          onTouchEnd={() => console.log('Clicou!')}
-          FrontIcon={ChevronDown}
-          />
-        <Button
-          title="00h00"
-          bgColor={colors.secondary.bg}
-          color={colors.white}
-          onTouchEnd={() => console.log('Clicou!')}
-          FrontIcon={ChevronDown}
-          />
-        <Button
-          title="13/02"
-          bgColor={colors.secondary.bg}
-          color={colors.white}
-          onTouchEnd={() => console.log('Clicou!')}
-          FrontIcon={ChevronDown}
+          size={itemSize}
         />
+        <TextInput
+          placeholder="Digite sua tarefa"
+          style={classes.inputTextCard}
+          placeholderTextColor={colors.bg[11]}
+          multiline={true}
+        />
+        <Trash
+          color={colors.white}
+          size={itemSize}
+        />
+      </View>
 
+      <View style={classes.bottomButtonsCard}>
+        <View style={classes.configButtonsCard}>
+          <Button
+            title="Baixa"
+            bgColor={colors.secondary.bg}
+            color={colors.white}
+            onTouchEnd={() => console.log('Clicou!')}
+            FrontIcon={ChevronDown}
+          />
+          <Button
+            title="00h00"
+            bgColor={colors.secondary.bg}
+            color={colors.white}
+            onTouchEnd={() => console.log('Clicou!')}
+            FrontIcon={ChevronDown}
+          />
+          <Button
+            title="13/02"
+            bgColor={colors.secondary.bg}
+            color={colors.white}
+            onTouchEnd={() => console.log('Clicou!')}
+            FrontIcon={ChevronDown}
+          />
+
+        </View>
         <Button
           bgColor={colors.main.bg}
           color={colors.bg[1]}
@@ -42,7 +63,7 @@ const Card = () => {
           FrontIcon={Play}
         />
       </View>
-    </>
+    </View>
   );
 }
 
--- components/Card/styles.js
+++ components/Card/styles.js
@@ -1,16 +1,42 @@
 import { StyleSheet } from "react-native";
 
 /** Aqui vão os estilos dos eslementos. Cada estilo pode ter um nome, tal qual "container". */
-const useStyles = (colors) => StyleSheet.create({
+const useStyles = ({colors, itemSize}) => StyleSheet.create({
   containerCard: {
     display: "flex",
-    flexDirection: 'row',
     backgroundColor: colors.bg[5],
-    width: 328,
+    width: 430,
+    borderRadius: 8,
+    padding: 12,
     height: 'max-content',
+    gap: 24
   },
-  text: {
-    color: colors.bg.text, // A cor do texto.
+  bottomButtonsCard: {
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  configButtonsCard: {
+    gap: 16,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  cardHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'flex-start',
+  },
+  inputTextCard: {
+    flex: 1,
+    alignItems: 'flex-start',
+    paddingRight: 16,
+    paddingLeft: 16,
+    fontSize: 16,
+    fontWeight: '600',
+    fontSize: itemSize,
+    color: colors.white,
+    height: 'max-content',
   },
 });
 
