2c099c1f47cbd9ad7b0aa2291e358814212b5f46
Neill-Ataide <newdonataide2k19@gmail.com>
2024-02-11 16:30:55 -0400

    Tamanho da caixa TextInput aumentado | Ajuste na posição da Logo

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 
 const App = () => {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
--- pages/Register/styles.js
+++ pages/Register/styles.js
@@ -37,7 +37,7 @@ const useStyles = (colors) => StyleSheet.create({
   },
   textInput: {
     width: '100%',
-    height: 40,
+    height: 50,
     borderWidth: 1,
     borderColor: '#cccccc',
     borderRadius: 5,
@@ -48,6 +48,7 @@ const useStyles = (colors) => StyleSheet.create({
   image_taskfy: {
     height: 100,
     width: 200,
+    marginTop: -10,
   }
 });
 
