7b64f705def8eb6d4866a2e185b3870f9474cf05
Mateus Santos <a.mateussm1.5@gmail.com>
2024-02-15 18:04:53 -0300

    Start with navigation

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,28 +1,32 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import Button from "../../components/Button";
 
 import { theme } from "../../config";
 import { colors as useColors } from "../../config/styles";
 import useStyles from "./styles";
-import { ChevronDown } from "lucide-react-native";
+
+import { useNavigation } from '@react-navigation/native';
 
 const Login = () => {
+  /** Navigator */
+  const navigation = useNavigation();
+
   /** As cores estão configuradas, basta usá-las dentro do arquivo styles.js */
   const colors = useColors(theme);
   const classes = useStyles(colors);
 
   return (
     /** Pra usar o estilo, basta chamar ele como no exemplo abaixo. */
-    
+
     <View style={classes.container}>
 
       <Text style={classes.text}>Login</Text>
 
       <Text style={classes.text_usuario}>Usuário</Text>
-      <TextInput 
-      style={classes.textInput} 
-      placeholder="Digite seu usuário"
-      placeholderTextColor="grey"
+      <TextInput
+        style={classes.textInput}
+        placeholder="Digite seu usuário"
+        placeholderTextColor="grey"
       ></TextInput>
 
       <Text style={classes.text_senha}>Senha</Text>
@@ -35,13 +39,13 @@ const Login = () => {
       </TextInput>
 
       <Button
-        onTouchEnd={() => console.log("Entrar no App")}
+        onTouchEnd={() => navigation.navigate('Home')}
         color={colors.bg.text}
         bgColor={colors.main.bg}
         title="entrar"
       />
       <TouchableOpacity
-        onPress={() => console.log('Criar conta')}>
+        onPress={() => navigation.navigate('Register')}>
         <Text style={classes.buttonText}>Criar conta</Text>
       </TouchableOpacity>
 
